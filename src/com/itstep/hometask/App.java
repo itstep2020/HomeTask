4c4b595f6075edd10936e48bf1e55374fbd3cb8d
Vitaliy Timchenko <vitaliy.timchenko@gmail.com>
2014-11-03 01:55:44 +0200

    Init project

---
+++ src/com/itstep/hometask/App.java
@@ -0,0 +1,12 @@
+package com.itstep.hometask;
+
+/**
+ * Main class to execute students classes.
+ * 
+ * @author PP-12JavaV2
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello asdasd");
+	}
+}
