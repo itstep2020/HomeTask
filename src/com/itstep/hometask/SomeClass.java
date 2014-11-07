9df6e86b26ca000e8ea0446c1823ee8940f3cd29
DmitriyPutrya <dima.putrya@gmail.com>
2014-11-07 21:48:27 +0200

    added  by Putrya D.S

---
+++ src/com/itstep/hometask/SomeClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.itstep.hometask;
+
+/**
+ *
+ * @author dimaputrya
+ */
+public class SomeClass implements Student{
+    
+private String txt = "Putrya Dmitriy";
+    
+    @Override
+    public void say() {
+       System.out.println(txt);
+    }
+    
+}
