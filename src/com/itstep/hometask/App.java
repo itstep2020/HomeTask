60e99f08b1aec4a34220f4b01e39627114f7592d
Dyachuk Sergei <dyack82@mail.ru>
2014-11-07 23:23:39 +0200

    Add musik

--- src/com/itstep/hometask/App.java
+++ src/com/itstep/hometask/App.java
@@ -1,4 +1,5 @@
 package com.itstep.hometask;
+import com.itstep.hometask.dyack.Sound;
 
 /**
  * Main class to execute student's classes.
@@ -11,10 +12,14 @@ public class App {
 	
 	static final Timchenko tim = new Timchenko();
 	static final Student Putrya_D_S = new SomeClass();
+    static final String dyack = "Dyachuk_Sergei";
         
 	public static void main(String[] args) {
 		System.out.println("Welcome fantastic team project!");
 		tim.say();
                 Putrya_D_S.say();
+
+        System.err.println(dyack + " >> Play >>>");
+        new Sound("src/resources/Silent_Hill.wav", 0, 2);// (path, sleep, replay)
 	}
 }
